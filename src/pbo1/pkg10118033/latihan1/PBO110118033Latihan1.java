0c7a1fb2da38ff8bc1e4396fc69bbf410a6bb710
IkrarAB <ikraranugrah26@gmail.com>
2019-09-23 22:19:22 +0700

    Initial commit

---
+++ src/pbo1/pkg10118033/latihan1/PBO110118033Latihan1.java
@@ -0,0 +1,24 @@
+package pbo1.pkg10118033.latihan1;
+
+/*
+ *
+ * @author
+ * NAMA  : Ikrar Anugrah Bastary
+ * KELAS : PBO1
+ * NIM   : 10118033
+ *
+ */
+
+public class PBO110118033Latihan1 {
+    public static void main(String[] args) {
+        System.out.println("BIODATA" + "\n" + "------------------");
+        System.out.println("NAMA          : Ikrar Anugrah Bastary");
+        System.out.println("ALAMAT        : Perum. Bumi Sadang Permai A5/49");
+        System.out.println("TEMPAT LAHIR  : Bandung");
+        System.out.println("TANGGAL LAHIR : 25 Januari 2000");
+        System.out.println("------------------");
+        System.out.println("HOBI  : Gaming, Learn something new, Hear Music");
+        System.out.println("MAKES : Sushi");
+        System.out.println("MIKES : Larutan Penyegar Cap 3 Foot");
+    }
+}
